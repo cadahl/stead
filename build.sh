9ed6cfcf593f4c4d81887dd3143a2d52e7db66dd
carl <devnull@localhost>
2011-07-14 08:31:42 +0200

    Rolled transfer into build script with -p option.

--- build.sh
+++ build.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
-rm *~
-xbuild /p:Configuration=Release All.sln
+
+PUBLISH=0
+while getopts ":p" opt; do
+  case $opt in
+    p)
+      PUBLISH=1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+                                    
+echo "Building"
+xbuild /verbosity:quiet /nologo /p:Configuration=Release All.sln
 rm -rf deploy/*
+
+echo "Copying"
+echo "  binaries"
 cp bin/Release/*.exe deploy/
 cp bin/Release/*.dll deploy/
+
+echo "  libmdwrkapi"
 cp -P libmdwrkapi/libmdwrkapi.so* deploy/ 
+
+echo "  libczmq"
 cp bin/external/libczmq.so deploy/
+
+echo "Compressing"
 cd deploy
-tar acvf stead.tar.bz2 *
+tar acvf stead.tar.bz2 * > /dev/null
 cd ..
+echo "  done"
+
+if [ $PUBLISH -eq 1 ]
+  then
+    echo "Publishing" 
+    ssh carl@adahl.dyndns.org cat < deploy/stead.tar.bz2 ">" /home/carl/stead/stead.tar.bz2
+    echo "  done"
+fi
+ 
--- transfer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sh build.sh
-ssh carl@adahl.dyndns.org cat < deploy/stead.tar.bz2 ">" /home/carl/stead/stead.tar.bz2
